887b4edb4b63015aa4a6a12ba45bd6e9003cb36b
guillot bertrand <guillotbertrand95@gmail.com>
2025-08-29 11:26:18 +0200

    rooute api ok

--- routes/ProfileRoutes.js
+++ routes/ProfileRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Profile from "../models/Profile.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
-
+import User from "../models/User.js";
 const router = express.Router();
 
 // GET : récupérer le profil de l'utilisateur connecté
@@ -28,6 +28,22 @@ router.put("/", authMiddleware, async (req, res) => {
 		res.json(updatedProfile);
 	} catch (err) {
 		res.status(400).json({ error: err.message });
+	}
+});
+router.delete("/", authMiddleware, async (req, res) => {
+	try {
+		// Supprimer le profil de l'utilisateur
+		await Profile.findOneAndDelete({ user: req.user.id });
+
+		// Supprimer l'utilisateur
+		await User.findByIdAndDelete(req.user.id);
+
+		res.json({ message: "Compte et profil supprimés avec succès" });
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({
+			message: "Erreur lors de la suppression du compte",
+		});
 	}
 });
 
